38d994b1376a7f63a8e6d413adbd6c591c478599
이다은 <jegalmin02@gmail.com>
2025-06-05 16:17:59 +0900

    OCR, 번역 UI 초안

---
+++ WorldStudy-v10/Views/TranslateAIView.swift
@@ -0,0 +1,18 @@
+//
+//  TranslateAIView.swift
+//  WorldStudy-v10
+//
+//  Created by 이다은 on 6/5/25.
+//
+
+import SwiftUI
+
+struct TranslateAIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TranslateAIView()
+}
