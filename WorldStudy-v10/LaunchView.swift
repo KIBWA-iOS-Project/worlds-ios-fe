2d508e2524214815a58754d3bc37eff953fae3d9
seohallo <seoohaii@gmail.com>
2025-06-09 16:22:00 +0900

    Feat: LaunchScreen 추가

---
+++ WorldStudy-v10/LaunchView.swift
@@ -0,0 +1,31 @@
+//
+//  LaunchView.swift
+//  WorldStudy-v10
+//
+//  Created by 이서하 on 6/9/25.
+//
+
+import SwiftUI
+
+struct LaunchView: View {
+    @State private var isActive = false
+
+    var body: some View {
+        if isActive {
+            ContentView() // 이미 정의된 ContentView 사용
+        } else {
+            Image("LaunchImage")
+                .resizable()
+                .scaledToFill()
+                .ignoresSafeArea()
+                .onAppear {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                        withAnimation {
+                            isActive = true
+                        }
+                    }
+                }
+        }
+    }
+}
+
--- WorldStudy-v10/WorldStudy_v10App.swift
+++ WorldStudy-v10/WorldStudy_v10App.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct WorldStudy_v10App: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LaunchView()
         }
     }
 }
